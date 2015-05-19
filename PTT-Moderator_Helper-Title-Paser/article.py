2af59b5f2537797d7d85171b844125dc2d225c96
Ching Lin <ikafire1230@gmail.com>
2015-05-20 01:13:47 +0800

    Modify article parsing key.

--- PTT-Moderator_Helper-Title-Paser/article.py
+++ PTT-Moderator_Helper-Title-Paser/article.py
@@ -20,22 +20,33 @@ def create_articles_from_file(filename):
     articles = []
     for entry in j:
         article = Article()
-        article.id = entry[u'1_ID']
-        article.author = entry[u'2_作者']
-        article.title = entry[u'3_標題']
-        article.date = entry[u'4_日期']
-        article.ip = entry[u'5_ip']
-        article.body = entry[u'6_內文']
-        article.link = entry[u'7_文章網址']
-        article.type = entry[u'8_發文/回文']
-        article.timestamp = entry[u'9_TimeStamp']
+        article.id = entry[u'ID']
+        article.author = entry[u'作者']
+        article.title = entry[u'標題']
+        article.date = entry[u'日期']
+        article.ip = entry[u'ip']
+        article.body = entry[u'內文']
+        article.link = entry[u'文章網址']
+        article.type = entry[u'發文/回文']
+        article.timestamp = entry[u'TimeStamp']
+        article.signature = entry[u'簽名檔']
         articles.append(article)
     return articles
 
 def main():
     articles = create_articles_from_file('data.json')
     for a in articles:
-        print(a.title)
+        print(u'ID: {0}'.format(a.id))
+        print(u'作者: {0}'.format(a.author))
+        print(u'標題: {0}'.format(a.title))
+        print(u'日期: {0}'.format(a.date))
+        print(u'IP: {0}'.format(a.ip))
+        print(u'內文: {0}'.format(a.body))
+        print(u'網址: {0}'.format(a.link))
+        print(u'發回文: {0}'.format(a.type))
+        print(u'Timestamp: {0}'.format(a.timestamp))
+        print(u'簽名檔: {0}'.format(a.signature))
+        
 
 if __name__ == '__main__':
     main()
