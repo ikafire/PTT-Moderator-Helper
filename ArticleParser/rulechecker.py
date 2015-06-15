146ab0e2aad4ca0924fccfcac36b718448937443
Yen Chao <jolico025@gmail.com>
2015-06-15 15:31:44 +0800

    Add Rule 1 and Rule 9.

--- ArticleParser/rulechecker.py
+++ ArticleParser/rulechecker.py
@@ -1,4 +1,50 @@
 import count
+import title_parse as title
+
+def rule_1(article, users_dict):
+    sentence = "版規一: \n"
+    violated = False
+
+    tag = title.check_tag(article, users_dict)
+    if tag < 0:
+        sentence += "文章請使用本看板所提供之分類，自訂分類或無分類之文章一律刪除。\n"
+        violated = True
+
+    author = article.author
+    # check news number
+    if users_dict[author][1] > 1:
+        if users_dict[author][4] < 1:
+            sentence += "新聞額度超貼 1 篇。以退文處理。\n"
+            violated = True
+            users_dict[author][4] += 1
+        else:
+            sentence += "新聞額度超貼 " + users_dict[author][1]-1 + " 篇, 給予水桶 " + users_dict[author][1]-1 + " 個月。\n"
+            violated = True
+
+    # Check ask posts
+    if users_dict[author][2] > 2:
+        if users_dict[author][4] < 1:
+            sentence += "問卦額度超貼 1 篇。以退文處理。\n"
+            violated = True
+            users_dict[author][4] += 1
+        else:
+            sentence += "問卦額度超貼 " + users_dict[author][2]-2 + " 篇, 給予水桶 " + users_dict[author][2]-2 + " 個月。\n"
+            violated = True
+
+    # Check transfer posts
+    if users_dict[author][3] > 1:
+        sentence += "轉錄文章限制一人一天一篇，超貼轉錄 " + users_dict[author][3]-1 + " 篇, 給予水桶 " + (users_dict[author][3]-1)*2 + " 週。"
+        violated = True
+    # Check total posts
+    if users_dict[author][0] > 5:
+        if users_dict[author][4] < 1:
+            sentence += "每日發文額度超貼 1 篇。以退文處理。\n"
+            violated = True
+            users_dict[author][4] += 1
+        else:
+            sentence += "每日發文額度超貼 " + users_dict[author][0]-5 + " 篇, 給予水桶 " + users_dict[author][0]-5 + " 個月。\n"
+            violated = True
+    return violated, sentence
 
 def rule_6(article):
     sentence = "版規六: \n"
@@ -7,7 +53,7 @@ def rule_6(article):
     if count.number_of_lines(article.signature) > 6:
         sentence += "文章簽名檔過長。刪除\n"
         violated = True
-    
+
     artCharsCount = count.art_chars(article.body)
     tradCharsCount = count.traditional_chinese_chars(article.body)
     if artCharsCount > tradCharsCount:
@@ -20,9 +66,9 @@ def rule_6(article):
         violated = True
 
     return violated, sentence
-        
 
-def rule_9(article):
+
+def rule_9(article, users_dict):
     sentence = "版規九: \n"
     violated = False
 
@@ -38,6 +84,10 @@ def rule_9(article):
         sentence += "滿二十個字，但使用電腦觀看為一行文，退回並水桶六個月\n"
         violated = True
 
+    if title.check_tag(article,users_dict) > 0:
+        sentence += "任意使用公告\n"
+        violated = True
+
 
     return violated, sentence
 
@@ -45,13 +95,20 @@ def rule_9(article):
 def main():
     import article
     articles = article.create_articles_from_file('data.json')
+    users_dict = {}
+    prev_date = ""
     for a in articles:
+        title.check_date(a, prev_date, users_dict)
+        title.check_author(a, users_dict)
+        violated1, sentence1 = rule_1(a, users_dict)
         violated6, sentence6 = rule_6(a)
-        violated9, sentence9 = rule_9(a)
-        violated = violated6 or violated9
+        violated9, sentence9 = rule_9(a, users_dict)
+        violated = violated1 or violated6 or violated9
         if violated:
             print(a.title)
-            print(a.body)
+            #print(a.body)
+        if violated1:
+            print (sentence1)
         if violated6:
             print(sentence6)
         if violated9:
--- ArticleParser/title_parse.py
+++ ArticleParser/title_parse.py
@@ -1,85 +1,46 @@
 # -*- coding: utf-8 -*-
-import json
+# users_dict = {} # key = user id; value = [總發文數, [新聞]數, [問卦]數, 轉錄文章數, 退文數]
+# self.moderator_list = ["meowmeowgo", "seabox", "APPLEin5566", "XXXXGAY"]
+# self.prev_date = "" # Post date of previous article
+import article
 
-# Parsing JSON format (Test data)
-articles = json.loads('[{"2_作者":"liveforhappy(a)","3_標題":"[問卦]有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"[問卦]有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"[問卦]有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"[新聞]有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"[新聞]有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"有沒有","4_日期":"Thu May 13 21:56:01 2015"},\
-                        {"2_作者":"liveforhappy(a)","3_標題":"[公告]有沒有","4_日期":"Thu May 13 21:56:01 2015"}]')
+def check_date(article, prev_date, users_dict):
+    # Check date
+    # If the date has changed, clear the user data.
+    if not article.date == prev_date:
+        users_dict.clear()
+        prev_date = article.date
 
-class TitleParser:
-    def __init__(self):
-        self.users_dict = {} # key = user id; value = [總發文數, [新聞]數, [問卦]數, 轉錄文章數, 退文數]
-        self.violations = {} # key = article id; value = [article_title, article_link, [violated_rules]]
-        self.moderator_list = ['meowmeowgo', 'seabox', 'APPLEin5566', 'XXXXGAY']
-        self.prev_date = '' # Post date of previous article
+def check_author(article, users_dict):
+    # Check the author
+    author = article.author
+    if author.find("(") > 0:
+        author = author[0 : author.find(")")]
+    if author in users_dict:
+        users_dict[author][0] += 1 # total_count increases by 1
+    else:
+        # Initialize user data
+        users_dict[author] = [1, 0, 0, 0, 0]
 
-    def add_violation(self, article, violated_rule):
-        if article.id in violations:
-            violations[article.id][2].append(violated_rule)
+def check_tag(article, users_dict):
+    moderator_list = ["meowmeowgo", "seabox", "APPLEin5566", "XXXXGAY"]
+    tag = article.get_tag()
+    author = article.author
+    if tag == "新聞":
+        users_dict[author][1] += 1
+        return 0
+    elif tag == "問卦":
+        users_dict[author][2] += 1
+        return 0
+    elif tag == "爆卦":
+        return 0
+    elif tag == "公告":
+        if not article.author in moderator_list:
+            return 1
         else:
-            violations[article.id] = [article.title, article.link, [violated_rule]]
-
-    def check_date(self, article):
-        # Check date
-        # If the date has changed, clear the user data.
-        if not article.date == prev_date:
-            users_dict.clear()
-            prev_date = article.date
-
-    def get_author_data(self,article):
-        # Check the author
-        author = article.author
-        if author.find('(') > 0:
-            author = author[0 : author.find('(')]
-        if author in users_dict:
-            users_dict[author][0] += 1 # total_count increases by 1
-        else:
-            # Initialize user data
-            users_dict[author] = [1, 0, 0, 0, 0]
-
-    def check_tag(self, article):
-        tag = article.tag
-        if tag == u'新聞':
-            users_dict[author][1] += 1
-        elif tag == u'問卦':
-            users_dict[author][2] += 1
-        elif tag == u'爆卦':
-            pass
-        elif tag == u'公告':
-            if not article.author in moderator_list:
-                add_violation(article, u'1-9-2 5. 任意使用公告、回應公告。')
-        elif tag == u'轉錄':
-             users_dict[author][3] += 1
-        else:
-           add_violation(article, u'1-1-4 文章請使用本看板所提供之分類，自訂分類或無分類之文章一律刪除。')
-
-    def check_post_num(self, article):
-        author = article.author
-        # check news number
-        if users_dict[author][1] == 2:
-            if users_dict[author][4] < 1:
-                add_violation(article, u'1-1-1 新聞額度超貼 1 篇。以退文處理。')
-                users_dict[author][4] += 1
-            else:
-                add_violation(article, u'1-1-1 新聞額度超貼', users_dict[author][1]-1 , u'篇。')
-        elif users_dict[author][1] > 2:
-            add_violation(article, u'1-1-1 新聞額度超貼', users_dict[author][1]-1 , u'篇。')
-        # Check ask posts
-        if users_dict[author][2] == 3:
-            if users_dict[author][4] < 1:
-                add_violation(article, u'1-1-1 問卦額度超貼 1 篇。以退文處理。')
-                users_dict[author][4] += 1
-            else:
-                add_violation(article, u'1-1-1 問卦額度超貼', users_dict[author][2]-2 , u'篇。')
-            elif users_dict[author][2] > 3:
-                add_violation(article, u'1-1-1 問卦額度超貼', users_dict[author][2]-2 , u'篇。')
-        # Check transfer posts
-        if users_dict[author][3]>1:
-            add_violation(article, u'1-1-5 轉錄文章限制一人一天一篇，超貼轉錄', users_dict[author][3]-1 ,u'篇, 水桶', (users_dict[author][3]-1)*2 , u'週。')
-        # Check total posts
-        if users_dict[author][0] > 5:
-            add_violation(article, u'1-1-1 每日發文額度超貼', users_dict[author][0]-5 , u'篇。')
+            return 0
+    elif tag == u"轉錄":
+         users_dict[author][3] += 1
+         return 0
+    else:
+       return -1
